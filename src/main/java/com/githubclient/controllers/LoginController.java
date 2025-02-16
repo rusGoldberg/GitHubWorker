85b4507a15b956a4e229dbf3ba1baf156750ae62
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-16 03:17:31 +0300

    Update LoginController.java

--- src/main/java/com/githubclient/controllers/LoginController.java
+++ src/main/java/com/githubclient/controllers/LoginController.java
@@ -1,1 +1,9 @@
+package com.githubclient.controllers;
 
+import com.githubclient.services.GitHubService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
