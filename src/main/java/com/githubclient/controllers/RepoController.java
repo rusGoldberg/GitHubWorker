5406b77e62a8530d5d2315dc221456ec045ed647
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-17 23:44:51 +0300

    Update RepoController.java

--- src/main/java/com/githubclient/controllers/RepoController.java
+++ src/main/java/com/githubclient/controllers/RepoController.java
@@ -1,1 +1,20 @@
+package com.githubclient.controllers;
 
+import com.githubclient.model.Repository;
+import com.githubclient.services.GitHubService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class RepoController {
+
+    // Аннотация @FXML используется для связывания элементов интерфейса, определенных в FXML-файле, с полями контроллера.
+    @FXML
+    private ListView<Repository> repoListView;
+
+    // Объект GitHubService для взаимодействия с API GitHub.
+    private GitHubService gitHubService;
+
+    // Список репозиториев, отображаемых в ListView.
+    private ObservableList<Repository> repositories;
