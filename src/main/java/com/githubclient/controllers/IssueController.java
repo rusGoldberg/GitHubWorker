f6c4cd0b076f3ff438060c53854d502fb5035059
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-18 14:58:02 +0300

    Update IssueController.java

--- src/main/java/com/githubclient/controllers/IssueController.java
+++ src/main/java/com/githubclient/controllers/IssueController.java
@@ -1,1 +1,10 @@
+package com.githubclient.controllers;
+
+import com.githubclient.model.Issue;
+import com.githubclient.services.GitHubService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 
