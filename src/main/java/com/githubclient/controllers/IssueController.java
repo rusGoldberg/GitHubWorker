5e6fda07303d11eb63652e7ae20d8fde2a483a3c
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-18 14:58:36 +0300

    Update IssueController.java

--- src/main/java/com/githubclient/controllers/IssueController.java
+++ src/main/java/com/githubclient/controllers/IssueController.java
@@ -7,4 +7,25 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
+public class IssueController {
+
+    // Аннотация @FXML используется для связывания элементов интерфейса, определенных в FXML-файле, с полями контроллера.
+    @FXML
+    private ListView<Issue> issueListView;
+
+    // Объект GitHubService для взаимодействия с API GitHub.
+    private GitHubService gitHubService;
+
+    // Список issues, отображаемых в ListView.
+    private ObservableList<Issue> issues;
+
+    /**
+     * Конструктор контроллера.
+     * Инициализирует объект GitHubService и список issues.
+     */
+    public IssueController() {
+        this.gitHubService = new GitHubService();
+        this.issues = FXCollections.observableArrayList();
+    }
+
 
