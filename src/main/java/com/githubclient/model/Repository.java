98b0ab8a168a589d75751ee295720397f3302501
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-20 17:44:33 +0300

    Update Repository.java

--- src/main/java/com/githubclient/model/Repository.java
+++ src/main/java/com/githubclient/model/Repository.java
@@ -32,4 +32,38 @@ public class Repository {
         this.owner = owner;
     }
 
+    // Геттеры и сеттеры для полей класса.
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
 
