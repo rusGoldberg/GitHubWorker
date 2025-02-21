852ec680fb11e7b2ba4abd36802ad3f8d1a23160
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-21 21:47:58 +0300

    Update Issue.java

--- src/main/java/com/githubclient/model/Issue.java
+++ src/main/java/com/githubclient/model/Issue.java
@@ -1,2 +1,12 @@
 package com.githubclient.model;
 
+/**
+ * Класс, представляющий issue в репозитории на GitHub.
+ */
+public class Issue {
+
+    // Идентификатор issue.
+    private String id;
+
+    // Заголовок issue.
+    private String title;
