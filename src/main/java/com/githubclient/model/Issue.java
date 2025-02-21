b80f188adc465685cfa71887b3aec97c9e778fc1
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-21 21:49:22 +0300

    Update Issue.java

--- src/main/java/com/githubclient/model/Issue.java
+++ src/main/java/com/githubclient/model/Issue.java
@@ -17,3 +17,17 @@ public class Issue {
     // Пользователь, создавший issue.
     private User creator;
 
+    /**
+     * Конструктор для создания нового объекта Issue.
+     *
+     * @param id      идентификатор issue
+     * @param title   заголовок issue
+     * @param body    описание issue
+     * @param creator пользователь, создавший issue
+     */
+    public Issue(String id, String title, String body, User creator) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.creator = creator;
+    }
