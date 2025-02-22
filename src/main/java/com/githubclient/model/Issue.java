c57d55730995c5bddaa3b3c3e9c3171c1704b201
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-22 12:38:32 +0300

    Update Issue.java

--- src/main/java/com/githubclient/model/Issue.java
+++ src/main/java/com/githubclient/model/Issue.java
@@ -31,3 +31,21 @@ public class Issue {
         this.body = body;
         this.creator = creator;
     }
+
+    // Геттеры и сеттеры для полей класса.
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
