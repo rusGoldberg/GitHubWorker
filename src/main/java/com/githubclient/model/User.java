689508fbdb064739bfc32eeefab08f4376507f9d
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-24 09:13:20 +0300

    Update User.java

--- src/main/java/com/githubclient/model/User.java
+++ src/main/java/com/githubclient/model/User.java
@@ -22,4 +22,21 @@ public class User {
         this.username = username;
     }
 
+    // Геттеры и сеттеры для полей класса.
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
