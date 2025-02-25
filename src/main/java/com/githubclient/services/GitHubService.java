1c102761d69bebdc91ef1122f70c3f2ec83ad29a
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-02-26 00:32:12 +0300

    Create GitHubService.java

---
+++ src/main/java/com/githubclient/services/GitHubService.java
@@ -0,0 +1,12 @@
+package com.githubclient.services;
+
+import com.githubclient.model.Issue;
+import com.githubclient.model.Repository;
+import com.githubclient.model.User;
+import com.githubclient.utils.HttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
