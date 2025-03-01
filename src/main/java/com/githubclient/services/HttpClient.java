0f0cd68e396821710cc68529f672bab16041c905
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-01 17:56:52 +0300

    Update HttpClient.java

--- src/main/java/com/githubclient/services/HttpClient.java
+++ src/main/java/com/githubclient/services/HttpClient.java
@@ -4,3 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+/**
+ * Утилитарный класс для выполнения HTTP-запросов.
+ */
+public class HttpClient {
+
