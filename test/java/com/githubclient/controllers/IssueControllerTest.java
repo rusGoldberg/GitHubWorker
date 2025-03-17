954591f22f56672b40d51ba82f922de71f2724c5
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-17 07:47:32 +0300

    Update IssueControllerTest.java

--- test/java/com/githubclient/controllers/IssueControllerTest.java
+++ test/java/com/githubclient/controllers/IssueControllerTest.java
@@ -3,3 +3,12 @@ package com.githubclient.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+class IssueControllerTest {
+
+    private IssueController issueController;
+
+    @BeforeEach
+    void setUp() {
+        issueController = new IssueController();
+    }
