ce11fe9e879e034fe994062db43f1d46903a4997
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-20 21:19:46 +0300

    Update HttpClientTest.java

--- test/java/com/githubclient/services/HttpClientTest.java
+++ test/java/com/githubclient/services/HttpClientTest.java
@@ -4,3 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+class HttpClientTest {
+
+    private HttpClient httpClient;
+
+    @BeforeEach
+    void setUp() {
+        httpClient = new HttpClient();
+    }
