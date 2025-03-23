179f769dd55bcb7a89efa0b5b1e94b808f158d0f
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-23 20:16:01 +0300

    Update RepositoryTest.java

--- test/java/com/githubclient/model/RepositoryTest.java
+++ test/java/com/githubclient/model/RepositoryTest.java
@@ -7,3 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class RepositoryTest {
 
     private Repository repository;
+
+    @BeforeEach
+    void setUp() {
+        User owner = new User("1", "testUser");
+        repository = new Repository("1", "TestRepo", "A test repository", owner);
+    }
