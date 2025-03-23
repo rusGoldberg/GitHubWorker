b3b91033923a25667638a3c3124de7be21305ad0
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-23 20:15:37 +0300

    Update RepositoryTest.java

--- test/java/com/githubclient/model/RepositoryTest.java
+++ test/java/com/githubclient/model/RepositoryTest.java
@@ -3,3 +3,7 @@ package com.githubclient.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+class RepositoryTest {
+
+    private Repository repository;
