5fb0cb03a08f0de95ac3debcedd6b8092859e776
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-23 20:16:29 +0300

    Update RepositoryTest.java

--- test/java/com/githubclient/model/RepositoryTest.java
+++ test/java/com/githubclient/model/RepositoryTest.java
@@ -13,3 +13,11 @@ class RepositoryTest {
         User owner = new User("1", "testUser");
         repository = new Repository("1", "TestRepo", "A test repository", owner);
     }
+
+    @Test
+    void testGetters() {
+        assertEquals("1", repository.getId());
+        assertEquals("TestRepo", repository.getName());
+        assertEquals("A test repository", repository.getDescription());
+        assertEquals("testUser", repository.getOwner().getUsername());
+    }
