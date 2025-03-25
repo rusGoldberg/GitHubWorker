b92cda7f805f011f5c6fa72dcdeb2bb879d2fabb
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-25 08:42:49 +0300

    Update IssueTest.java

--- test/java/com/githubclient/model/IssueTest.java
+++ test/java/com/githubclient/model/IssueTest.java
@@ -13,3 +13,11 @@ class IssueTest {
         User creator = new User("1", "testUser");
         issue = new Issue("1", "Test Issue", "This is a test issue", creator);
     }
+
+    @Test
+    void testGetters() {
+        assertEquals("1", issue.getId());
+        assertEquals("Test Issue", issue.getTitle());
+        assertEquals("This is a test issue", issue.getBody());
+        assertEquals("testUser", issue.getCreator().getUsername());
+    }
