140ef6ea11364750963aba765b583a7e33787754
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-25 08:42:24 +0300

    Update IssueTest.java

--- test/java/com/githubclient/model/IssueTest.java
+++ test/java/com/githubclient/model/IssueTest.java
@@ -3,3 +3,13 @@ package com.githubclient.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+class IssueTest {
+
+    private Issue issue;
+
+    @BeforeEach
+    void setUp() {
+        User creator = new User("1", "testUser");
+        issue = new Issue("1", "Test Issue", "This is a test issue", creator);
+    }
