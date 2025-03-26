fbffdc54ae9c9b96faf605e4338f17287a36446b
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-03-26 22:25:30 +0300

    Update UserTest.java

--- test/java/com/githubclient/model/UserTest.java
+++ test/java/com/githubclient/model/UserTest.java
@@ -3,3 +3,13 @@ package com.githubclient.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User("1", "testUser");
+    }
+
