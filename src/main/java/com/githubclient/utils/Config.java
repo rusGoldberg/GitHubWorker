00483aced86faa6162a1a1cbe9cf647602be108c
rusGoldberg <92647335+rusGoldberg@users.noreply.github.com>
2025-04-11 00:53:07 +0300

    Update Config.java

--- src/main/java/com/githubclient/utils/Config.java
+++ src/main/java/com/githubclient/utils/Config.java
@@ -32,3 +32,7 @@ public class Config {
         return authToken;
     }
 }
+/*
+1. Пакет и импорт:
+package com.githubclient.utils;: Указывает, что этот класс находится в пакете com.githubclient.utils.
+*/
